c1495e03881c29829a311c2f18c8f0b1d7daeb86
dagui <davaguir852@gmail.com>
2023-06-12 23:39:32 -0700

    finished part 9

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,11 +1,20 @@
 const React = require('react')
 const Def = require('../default')
 
-function new_form () {
+function new_form (data) {
+    let message = ''
+    if(data.message){
+      message = (
+        <h4 className='alert-danger'>
+          {data.message}
+        </h4>
+      )
+    }
     return (
         <Def>
             <main>
                 <h1>Add a New Place</h1>
+                {message}
                 <form method="POST" action="/places">
                     <div className="form-group">
                         <label htmlFor="name">Place Name</label>
@@ -28,8 +37,8 @@ function new_form () {
                         <input className="form-control" id="cuisines" name="cuisines" required/>
                     </div>
                     <div className="form-group">
-                        <label for="founded">Founded Year</label>
-                        <input className="form-control" id="founded" name="founded" />
+                        <label htmlFor="founded">Founded Year</label>
+                        <input type='number' className="form-control" id="founded" name="founded" value={new Date().getFullYear()} />
                     </div>
                     <input className="btn btn-primary" type="submit" value="Add Place"/>
                 </form>
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -25,7 +25,7 @@ function show (data) {
               </div>
               <a href={`/places/${data.id}/edit`} className="btn btn-warning"> Edit</a>
              <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}> 
-                <button type="submit" className="btn btn-danger">Delete</button>
+               <button type="submit" className="btn btn-danger">Delete</button>
               </form>
           </div>   
           </main>
