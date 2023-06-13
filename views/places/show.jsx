3a06a5679fd7e6fd3af496647ba6f1a2f50588c4
dagui <davaguir852@gmail.com>
2023-06-12 23:05:56 -0700

    created and used helper method

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,11 +5,29 @@ function show (data) {
     return (
         <Def>
           <main>
-            <h1>{ data.place.name }</h1>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> Edit</a>
-        <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}> 
-            <button type="submit" className="btn btn-danger">Delete</button>
-        </form>     
+            <div className='row'>
+              <div className='col-sm-6'>
+                <img src={data.place.pic} alt={data.place.name} />
+                <h3>
+                  Located in {data.place.city}, {data.place.state}
+                </h3>
+              </div>
+              <div className='col-sm-6'>
+                <h2>
+                  Description
+                </h2>
+                <h3>
+                  {data.place.showEstablished()}
+                </h3>
+                <h4>
+                  Serving {data.place.cuisines}
+                </h4>
+              </div>
+              <a href={`/places/${data.id}/edit`} className="btn btn-warning"> Edit</a>
+             <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}> 
+                <button type="submit" className="btn btn-danger">Delete</button>
+              </form>
+          </div>   
           </main>
         </Def>
     )
